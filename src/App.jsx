3943889e425bd29ad0af151d945164cc749cae93
Oksana Kolesova <kolesovaok85@gmail.com>
2025-02-01 11:33:13 +0200

    hw4

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,114 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import css from './App.module.css';
+import fetchGalleryImage from './components/api/api';
 
-import ContactForm from './components/ContactForm/ContactForm';
-import ContactList from './components/ContactList/ContactList';
-import SearchBox from './components/SearchBox/SearchBox';
-import contactsData from './contacts.json';
-
-const ContactsKey = 'initial-contacts';
-
-const initialContact = () => {
-  const localStorageContacts = localStorage.getItem(ContactsKey);
-  return localStorageContacts ? JSON.parse(localStorageContacts) : contactsData;
-};
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import ImageModal from './components/ImageModal/ImageModal';
+import Loader from './components/Loader/Loader';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
-  const [contacts, setContacts] = useState(initialContact);
+  const [gallery, setGallery] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const [search, setSearch] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+  const [description, setDescription] = useState('');
+
+  const referation = useRef(null);
 
   useEffect(() => {
-    localStorage.setItem(ContactsKey, JSON.stringify(contacts));
-  }, [contacts]);
+    if (!query) return;
 
-  const addContact = newContact => {
-    if (
-      contacts.some(
-        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-      )
-    ) {
-      alert(`${newContact.name} is already in your contacts.`);
-      return;
+    const handleSearch = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const data = await fetchGalleryImage(query, page);
+        // console.log('data:', data);
+        if (data.total === 0) return;
+        setGallery(prevGallery => {
+          return [...prevGallery, ...data.results];
+        });
+        setTotalPage(data.total_page);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    handleSearch();
+  }, [page, query]);
+
+  useEffect(() => {
+    if (page === 1) return;
+
+    if (referation.current) {
+      referation.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
     }
-    setContacts(prevContacts => [...prevContacts, newContact]);
+  }, [page, gallery]);
+
+  const handleQuery = newQuery => {
+    setQuery(newQuery);
+    setGallery([]);
+    setPage(1);
   };
 
-  const deleteContact = id => {
-    setContacts(prevContacts => {
-      return prevContacts.filter(contacts => contacts.id !== id);
-    });
+  const openModal = () => {
+    setModalOpen(true);
   };
 
-  const filterContact = search
-    ? contacts.filter(contact =>
-        contact.name.toLowerCase().includes(search.toLowerCase())
-      )
-    : contacts;
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const upgradeModalData = (scr, alt) => {
+    setModalImage(scr);
+    setDescription(alt);
+  };
+
+  const active = useMemo(() => page === totalPage, [page, totalPage]);
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={search} onSearch={setSearch} />
-      <ContactList contacts={filterContact} onDelete={deleteContact} />
+    <div>
+      <SearchBar onSubmit={handleQuery} />
+      {gallery.length > 0 && (
+        <ImageGallery
+          gallery={gallery}
+          openModal={openModal}
+          upgradeModalData={upgradeModalData}
+        />
+      )}
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {gallery.length > 0 && !loading && !error && (
+        <LoadMoreBtn handleLoadMore={handleLoadMore} isActive={active} />
+      )}
+      <ImageModal
+        modalOpen={modalOpen}
+        modalClose={closeModal}
+        src={modalImage}
+        alt={description}
+      />
+      <div ref={referation}></div>
+      <Toaster
+        position="top-right"
+        reverseOrder={true}
+        toastOptions={{
+          className: css.toastTextCenter,
+        }}
+      />
     </div>
   );
 };
